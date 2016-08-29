915fca05bcf60f452e2ef016cefc1fb99e82b699
Tyler Warren <warrejt@yahoo.com>
2016-08-29 12:00:32 -0500

    added patients migration

---
+++ database/migrations/2016_08_29_163049_create_patients_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePatientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('patients', function (Blueprint $table) {
+            $table->increments('id')->unsigned();
+            $table->integer('user_id')->unsigned();
+            $table->string('first_name', 20);
+            $table->string('last_name', 20);
+            $table->integer('ssn')->unsigned();
+            $table->string('emergency_contact_name');
+            $table->integer('emergency_contact_number');
+            $table->string('emergency_contact_email', 20);
+            $table->string('medication');
+            $table->string('health_insurance');
+            $table->rememberToken();
+            $table->timestamps();
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
