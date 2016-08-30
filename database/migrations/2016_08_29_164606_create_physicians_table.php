15f9943c7a5cd74630dff8ddc004c01b18444078
Tyler Warren <warrejt@yahoo.com>
2016-08-30 15:04:25 -0500

    adding physician_id column to patients table; deleting patient_id from physicians table ;

--- database/migrations/2016_08_29_164606_create_physicians_table.php
+++ database/migrations/2016_08_29_164606_create_physicians_table.php
@@ -16,11 +16,9 @@ class CreatePhysiciansTable extends Migration
             $table->increments('id')->unsigned();
             $table->integer('user_id')->unsigned();
             $table->integer('npi')->unsigned()->unique();
-            $table->integer('patient_id')->unsigned();
             $table->integer('clinic')->unsigned();
             $table->timestamps();
             $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('patient_id')->references('id')->on('patients');
         });
     }
 
