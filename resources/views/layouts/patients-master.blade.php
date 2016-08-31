d2f59592d2d6344272dfa37f60ec4e9729accbff
Tyler Warren <warrejt@yahoo.com>
2016-08-31 12:22:10 -0500

    adding basic navbar to patients master blade

--- resources/views/layouts/patients-master.blade.php
+++ resources/views/layouts/patients-master.blade.php
@@ -7,7 +7,22 @@
     crossorigin="anonymous">
 </head>
 <body>
-	 @yield('content')
+	@yield('content')
+	<nav class="navbar navbar-default">
+	  <div class="container-fluid">
+	    <div class="navbar-header">
+	      <a class="navbar-brand" href="#">
+	        <img alt="Galapago" src="...">
+	      </a>
+	    </div>
+	    <div class="navbar-header navbar-right">
+	    	<ul class="nav navbar-nav">
+	    		<li><a href="#">Help</a></li>
+	    	</ul>
+	    	<button action="" type="button" class="btn btn-default navbar-btn">Sign Out</button>
+	    </div>
+	  </div>
+	</nav>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" 
     integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" 
     crossorigin="anonymous"></script>
