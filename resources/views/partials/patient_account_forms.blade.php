b5f1faf5ad9a92adb65aa61d37cba22065a021ff
Tyler Warren <warrejt@yahoo.com>
2016-09-01 16:59:04 -0500

    style the patients master, making responsive to mobile, added style sheet for patients master

--- resources/views/partials/patient_account_forms.blade.php
+++ resources/views/partials/patient_account_forms.blade.php
@@ -1,7 +1,7 @@
 @forelse ($patient->submission as $submission)
 <div class"row">
-	<div class="col-md-4 col-md-offset-2"> Form No. {{ $submission->form_id }}</div>
-  	<div class="col-md-4 col-md-offset-2"><a class="btn btn-primary" href="#">View Form No. {{ $submission->form_id }}</a></div>
+	<div class="col-md-4 col-md-offset-2 col-xs-5 col-xs-offset-3"> Form No. {{ $submission->form_id }}</div>
+  	<div class="col-md-4 col-md-offset-2 col-xs-4"><a class="btn btn-primary" href="#">View Form No. {{ $submission->form_id }}</a></div>
 </div>
 @empty
 <div class"row">
