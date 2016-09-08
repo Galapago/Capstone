cf13c7081ff395a5844fb5c8da047abae9259b0f
Tyler Warren <warrejt@yahoo.com>
2016-09-08 10:26:19 -0500

    added additional columns to patients table

--- database/migrations/2016_08_29_164606_create_patients_table.php
+++ database/migrations/2016_08_29_164606_create_patients_table.php
@@ -15,15 +15,17 @@ class CreatePatientsTable extends Migration
         Schema::create('patients', function (Blueprint $table) {
             $table->increments('id')->unsigned();
             $table->integer('user_id')->unsigned();
+            $table->foreign('user_id')->references('id')->on('users');
             $table->integer('physician_id')->unsigned();
+            $table->foreign('physician_id')->references('id')->on('physicians');
             $table->string('first_name', 30);
             $table->string('last_name', 30);
             $table->string('street_address', 100);
             $table->string('apt_ste', 20);
             $table->string('city', 20);
             $table->string('state', 20);
-            $table->integer('zip_code', 15)->unsigned();
-            $table->date('date');
+            $table->integer('zip_code')->length(15)->unsigned();
+            $table->date('dob');
             $table->integer('height')->unsigned();
             $table->integer('weight')->unsigned();
             $table->integer('phone')->unsigned();
@@ -34,8 +36,6 @@ class CreatePatientsTable extends Migration
             $table->string('medication');
             $table->string('health_insurance');
             $table->timestamps();
-            $table->foreign('user_id')->references('id')->on('users');
-            $table->foreign('physician_id')->references('id')->on('physicians');
         });
     }
 
