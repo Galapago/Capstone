5a7ea18ca4a921071ca6abdb593d70ff7bca3b29
Tyler Warren <warrejt@yahoo.com>
2016-09-08 11:30:52 -0500

    got patients account page to link to each form the patient has filled out

--- resources/views/partials/patient_account_forms.blade.php
+++ resources/views/partials/patient_account_forms.blade.php
@@ -1,7 +1,7 @@
 @forelse ($patient->submissions as $submission)
 <div class"row">
 	<div class="col-md-4 col-md-offset-2 col-xs-5 col-xs-offset-3"> Form No. {{ $submission->form_id }}</div>
-  	<div class="col-md-4 col-md-offset-2 col-xs-4"><a class="btn btn-primary" href="#">View Form No. {{ $submission->form_id }}</a></div>
+  	<div class="col-md-4 col-md-offset-2 col-xs-4"><a class="btn btn-primary" href="{{ action('SubmissionsController@show', $submission->id) }}">View Form No. {{ $submission->form_id }}</a></div>
 </div>
 @empty
 <div class"row">
