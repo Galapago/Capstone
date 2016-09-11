89f8feadfed93948e353dd1900072aae149b9c5b
Tyler Warren <warrejt@yahoo.com>
2016-09-11 16:36:22 -0500

    styling physicians

--- resources/views/partials/send_form.blade.php
+++ resources/views/partials/send_form.blade.php
@@ -1,23 +1,25 @@
-<form>
-	<label>Choose a Form:</label><br>
-	@foreach ($forms as $form)
-	<div>
-		<input
-			type="radio"
-			name="{{ $form->id }}" 
-			value="{{ $form->id }}"> {{ $form->form_name }}
-		
-	</div>
-	@endforeach
+<div class="container">
+	<form>
+		<label>Choose a Form:</label><br>
+		@foreach ($forms as $form)
+		<div>
+			<input
+				type="radio"
+				name="{{ $form->id }}" 
+				value="{{ $form->id }}"> {{ $form->form_name }}
+			
+		</div>
+		@endforeach
 
-	<label>Which Patient:</label><br>
-	@foreach ($patients as $patient)
-	<div>
-		<input
-			type="radio"
-			name="{{ $patient->name }}" 
-			value="{{ $patient->id }}"> {{ $patient->first_name . " " . $patient->last_name }}
-	</div>
-	@endforeach
-	<button type="submit">Send</button>
-</form>
+		<label>Which Patient:</label><br>
+		@foreach ($patients as $patient)
+		<div>
+			<input
+				type="radio"
+				name="{{ $patient->name }}" 
+				value="{{ $patient->id }}"> {{ $patient->first_name . " " . $patient->last_name }}
+		</div>
+		@endforeach
+		<button type="submit">Send</button>
+	</form>
+</div>
--- resources/views/physicians/show.blade.php
+++ resources/views/physicians/show.blade.php
@@ -1,12 +1,15 @@
 @extends('layouts.physicians-master')
 
 @section('content')
+
+
+<div class="header">
+    <h1 class="section-title">Welcome to GalapaGo!</h1>
+    <h2>Account For Dr. {{ $physician->first_name }} {{ $physician->last_name }}</h2><br>
+</div>
+@include('partials.send_form')
 <div class="container">
 	<section>
-    <div class="header">
-    	<h1 class="section-title">Welcome to GalapaGo!</h1>
-    	<h2>Account For Dr. {{ $physician->first_name }} {{ $physician->last_name }}</h2><br>
-    </div>
 	<table class="table table-hover">
         <thead>
             <tr>
@@ -22,6 +25,6 @@
     </table>
 	</section>
 </div>	
-@include('partials.send_form')
+
 
 @stop
