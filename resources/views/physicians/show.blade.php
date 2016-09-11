e5d8c293a2413476f586a0fb8e5d110d0f2f1b00
Tyler Warren <warrejt@yahoo.com>
2016-09-11 18:56:28 -0500

    added style to physicians account page
    DELETED METHOD FROM SEND_FORMS BLADE - ADD BACK!!

--- resources/views/physicians/show.blade.php
+++ resources/views/physicians/show.blade.php
@@ -3,12 +3,15 @@
 @section('content')
 
 
-<div class="header">
+<div class="form-group header">
     <h1 class="section-title">Welcome to GalapaGo!</h1>
-    <h2>Account For Dr. {{ $physician->first_name }} {{ $physician->last_name }}</h2><br>
+    <h3>Account For Dr. {{ $physician->first_name }} {{ $physician->last_name }}</h3>
 </div>
 @include('partials.send_form')
 <div class="container">
+    <div class="form-group">
+        <h3 class="header">Completed Patient Forms</h3>
+    </div>
 	<section>
 	<table class="table table-hover">
         <thead>
