e83e49dc72d771b62ce4310b37bc437060970f67
Tyler Warren <warrejt@yahoo.com>
2016-09-11 16:29:10 -0500

    styling

--- resources/views/physicians/show.blade.php
+++ resources/views/physicians/show.blade.php
@@ -3,9 +3,10 @@
 @section('content')
 <div class="container">
 	<section>
-	<h1 class="section-title">Welcome to GalapaGo!</h1>
-	<h2>Account For Dr. {{ $physician->first_name }} {{ $physician->last_name }}</h2><br>
-
+    <div class="header">
+    	<h1 class="section-title">Welcome to GalapaGo!</h1>
+    	<h2>Account For Dr. {{ $physician->first_name }} {{ $physician->last_name }}</h2><br>
+    </div>
 	<table class="table table-hover">
         <thead>
             <tr>
